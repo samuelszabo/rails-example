01c1f4f1d7d7f60dfffc2c2b7bc7c8b8d22f9628
Samuel Szabo <samuelszabo7+git@gmail.com>
2019-12-11 18:53:37 +0100

    more and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new()
   end
 
   def create
@@ -17,6 +22,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
